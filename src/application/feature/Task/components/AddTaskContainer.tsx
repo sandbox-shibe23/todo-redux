f87351f3c781683970008c9337674d6a9a30329a
shibe23 <shibe23.info@gmail.com>
2020-12-02 23:55:11 +0900

    feat: modify AddTaskContainer.tsx

--- src/application/feature/Task/components/AddTaskContainer.tsx
+++ src/application/feature/Task/components/AddTaskContainer.tsx
@@ -2,13 +2,14 @@ import { useDispatch } from 'react-redux';
 import React from 'react';
 import { addTask } from '../../../actions/TaskActions';
 
-export function TaskContainer() {
+export function AddTaskContainer() {
     const dispatch = useDispatch();
     const ref = React.useRef<HTMLInputElement>(null);
 
     const handleClick = () => {
         if(ref.current !== null) {
             dispatch(addTask(ref.current.value));
+            ref.current.value = ''
         }
     };
 
