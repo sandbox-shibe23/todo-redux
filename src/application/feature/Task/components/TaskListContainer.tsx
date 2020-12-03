7eafe27afe5cd60a21745bb051835094d3dd3acb
shibe23 <shibe23.info@gmail.com>
2020-12-03 23:20:04 +0900

    feat: Filter by Status.

--- src/application/feature/Task/components/TaskListContainer.tsx
+++ src/application/feature/Task/components/TaskListContainer.tsx
@@ -4,9 +4,11 @@ import { useTypedSelector } from '../../../AppicationStore';
 export function TaskListContainer() {
     // ts-expect-error
     const tasks = useTypedSelector((state) => state.tasks);
+    const notDoneTasks = tasks.filter(task => task.status === 'ready')
+
     return (
         <div>
-            {tasks.map((task) => {
+            {notDoneTasks.map((task) => {
                 return (
                     <div key={task.id}>
                         <span>{task.text}</span> :<span>{task.status}</span>
