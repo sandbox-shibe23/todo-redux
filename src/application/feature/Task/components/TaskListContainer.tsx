344c377021377ff369471b422f48ee774c1bf3a5
shibe23 <shibe23.info@gmail.com>
2020-12-05 23:10:59 +0900

    style: Reformat Code.

--- src/application/feature/Task/components/TaskListContainer.tsx
+++ src/application/feature/Task/components/TaskListContainer.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import { useTypedSelector } from '../../../AppicationStore';
-import {showEditInput} from "../../../actions/TaskActions";
-import {useDispatch} from "react-redux";
+import { showEditInput } from '../../../actions/TaskActions';
+import { useDispatch } from 'react-redux';
 
 export function TaskListContainer() {
     // ts-expect-error
     const tasks = useTypedSelector((state) => state.tasks);
-    const notDoneTasks = tasks.filter(task => task.status === 'ready')
-    const dispatch = useDispatch()
+    const notDoneTasks = tasks.filter((task) => task.status === 'ready');
+    const dispatch = useDispatch();
 
     const handleClick = (taskID: number) => {
         dispatch(showEditInput(taskID));
-    }
+    };
 
     return (
         <div>
             {notDoneTasks.map((task) => {
                 return (
                     <div key={task.id}>
-                        <span>{task.text}</span> :<button type="button" onClick={() => handleClick(task.id)}>{task.status}</button>
+                        <span>{task.text}</span> :
+                        <button
+                            type="button"
+                            onClick={() => handleClick(task.id)}
+                        >
+                            {task.status}
+                        </button>
                     </div>
                 );
             })}
         </div>
     );
 }
-
