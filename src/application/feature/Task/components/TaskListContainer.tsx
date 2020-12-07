f8cd9b26382dab5fdd34f911a118e4c0f00efd53
shibe23 <shibe23.info@gmail.com>
2020-12-07 23:55:10 +0900

    feat: EditButtonを編集状態によって切り替え

--- src/application/feature/Task/components/TaskListContainer.tsx
+++ src/application/feature/Task/components/TaskListContainer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTypedSelector } from '../../../AppicationStore';
-import { showEditInput } from '../../../actions/TaskActions';
 import { useDispatch } from 'react-redux';
 import { Task, taskSelector, taskSlice } from '../TaskSlices';
 
@@ -10,19 +9,47 @@ export function TaskListContainer() {
     const tasks = useTypedSelector((state) => state.tasks);
     const allTasks = taskSelector.selectAll(tasks);
     const dispatch = useDispatch();
-    const handleClick = () => {};
+
+    const [editTaskID, setEditTaskID] = React.useState<number | null>(null);
+
+    const handleClick = (taskID: number) => {
+        if (taskID === editTaskID) {
+            setEditTaskID(null);
+        } else {
+            setEditTaskID(taskID);
+        }
+    };
     return (
         <div>
             {allTasks.map((task) => {
                 return (
                     <div key={task.taskID}>
-                        <span>{task.text}</span> :
-                        <button type="button" onClick={() => handleClick()}>
-                            {task.status}
-                        </button>
+                        <span>{task.text}</span>
+                        <EditButton
+                            task={task}
+                            editTaskID={editTaskID}
+                            handleClick={handleClick}
+                        />
                     </div>
                 );
             })}
         </div>
+    );
+}
+
+interface EditButtonProps {
+    task: Task;
+    editTaskID: number | null;
+    handleClick: (task: number) => void;
+}
+
+function EditButton(props: EditButtonProps) {
+    const { task, editTaskID, handleClick } = props;
+    return (
+        <>
+            <button type="button" onClick={() => handleClick(task.taskID)}>
+                {editTaskID === task.taskID ? '決定' : '編集'}
+            </button>
+        </>
     );
 }
